b162e313539c8e4f3caa609a815790f65692ccf7
Pedro <pedro.abucarma04@gmail.com>
2024-05-07 18:11:50 -0300

    project start
    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# stocks_monitor
+ A dashboard for the user to track their stock portfolio.
---
+++ components/modal.py
@@ -0,0 +1,3 @@
+from dash import dcc, Input, Output, State, no_update, callback_context
+import dash_bootstrap_components as dbc
+from datetime import date
